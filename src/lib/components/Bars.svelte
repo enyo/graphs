3c2f31257eaf9db2e5b908eacd8a929a41e78bc0
Matias Meno <m@tias.me>
2023-09-01 15:12:07 +0200

    Cleanup

--- src/lib/components/Bars.svelte
+++ src/lib/components/Bars.svelte
@@ -59,92 +59,104 @@
   }
 </script>
 
-<div>
-  Your data:<br />
-  <textarea bind:value={data} />
+Your data:<br />
+<textarea bind:value={data} />
 
-  {#if parsed}
-    <svg
-      bind:this={svgElement}
-      viewBox="0 0 {width} {height}"
-      {width}
-      {height}
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      {#each parsed as values, i}
-        {@const y = i * (rowHeight + gap)}
-        <g transform="translate({getWidth(width, max1 - parseInt(values[0]))}, 0)">
-          <rect x="0" {y} fill={color1} width={getWidth(width, values[0])} height={rowHeight} />
-          <rect
-            fill={color2}
-            x={getWidth(width, values[0])}
-            {y}
-            width={getWidth(width, values[1])}
-            height={rowHeight}
-          />
-          <rect
-            x={getWidth(width, values[0]) - getWidth(width, values[2])}
-            {y}
-            fill={color3}
-            width={getWidth(width, values[2])}
-            height={rowHeight}
-          />
-          <rect
-            x={getWidth(width, values[0])}
-            {y}
-            fill={color4}
-            width={getWidth(width, values[3])}
-            height={rowHeight}
-          />
-        </g>
-      {/each}
-    </svg>
-  {/if}
-
-  <button on:click={downloadSvg}>Download</button>
-
-  <!-- <pre>{svg}</pre> -->
-</div>
-
-<div class="settings">
-  <label>
-    Color 1: <input type="color" bind:value={color1} />
-  </label>
-  <label>
-    Color 2: <input type="color" bind:value={color2} />
-  </label>
-  <label>
-    Color 3: <input type="color" bind:value={color3} />
-  </label>
-  <label>
-    Color 4: <input type="color" bind:value={color4} />
-  </label>
-  <label>
-    Height: <input type="range" min="10" max="100" bind:value={rowHeight} />
-  </label>
-  <label>
-    Width: <input type="range" min="100" max="1000" bind:value={width} />
-  </label>
-  <label>
-    Gap: <input type="range" min="0" max="30" bind:value={gap} />
-  </label>
-</div>
+{#if parsed}
+  <div class="graph">
+    <div class="actions">
+      <div class="settings">
+        <label>
+          Color 1: <input type="color" bind:value={color1} />
+        </label>
+        <label>
+          Color 2: <input type="color" bind:value={color2} />
+        </label>
+        <label>
+          Color 3: <input type="color" bind:value={color3} />
+        </label>
+        <label>
+          Color 4: <input type="color" bind:value={color4} />
+        </label>
+        <label>
+          Height: <input type="range" min="10" max="100" bind:value={rowHeight} />
+          <input type="number" bind:value={rowHeight} />
+        </label>
+        <label>
+          Width: <input type="range" min="100" max="1000" bind:value={width} />
+          <input type="number" bind:value={width} />
+        </label>
+        <label>
+          Gap: <input type="range" min="0" max="30" bind:value={gap} />
+          <input type="number" bind:value={gap} />
+        </label>
+      </div>
+      <button on:click={downloadSvg}>Download</button>
+    </div>
+    <div class="svg">
+      <svg
+        bind:this={svgElement}
+        viewBox="0 0 {width} {height}"
+        {width}
+        {height}
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        {#each parsed as values, i}
+          {@const y = i * (rowHeight + gap)}
+          <g transform="translate({getWidth(width, max1 - parseInt(values[0]))}, 0)">
+            <rect x="0" {y} fill={color1} width={getWidth(width, values[0])} height={rowHeight} />
+            <rect
+              fill={color2}
+              x={getWidth(width, values[0])}
+              {y}
+              width={getWidth(width, values[1])}
+              height={rowHeight}
+            />
+            <rect
+              x={getWidth(width, values[0]) - getWidth(width, values[2])}
+              {y}
+              fill={color3}
+              width={getWidth(width, values[2])}
+              height={rowHeight}
+            />
+            <rect
+              x={getWidth(width, values[0])}
+              {y}
+              fill={color4}
+              width={getWidth(width, values[3])}
+              height={rowHeight}
+            />
+          </g>
+        {/each}
+      </svg>
+    </div>
+  </div>
+{/if}
 
 <style lang="postcss">
   textarea {
     width: 20rem;
     height: 5rem;
   }
+  .svg {
+    padding: 1.5rem;
+    background: #fafafa;
+  }
   svg {
     display: block;
-    margin: 3rem 0;
   }
   button {
     padding: 1rem 2rem;
     font-size: 2rem;
   }
+  .graph {
+    display: flex;
+    gap: 2rem;
+    flex-wrap: wrap;
+    align-items: flex-start;
+  }
   .settings {
-    margin-top: 3rem;
+    margin-bottom: 3rem;
   }
   label {
     display: block;
